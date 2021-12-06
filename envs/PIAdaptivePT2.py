5b10c913cbc4aa01de21e8ce90301a3cce6cf151
Julius <julius.brandl97@gmail.com>
2021-12-06 10:04:30 +0100

    change to use new TfSim.py

--- envs/PIAdaptivePT2.py
+++ envs/PIAdaptivePT2.py
@@ -1,171 +1,197 @@
-import random
+import time
+from collections import deque
 
 import control
 import gym
 import matplotlib.pyplot as plt
 import numpy as np
+from envs.TfSim import TfSim
 
 
 class PIAdaptivePT2(gym.Env):
-    def __init__(self, oscillating=False, model_sample_frequency=10_000, controller_sample_frequency=100,
-                 simulation_time=1.5):
+
+    def __init__(self, oscillating=True, log=False):
         super(PIAdaptivePT2, self).__init__()
         if oscillating:
-            self.sys = control.tf([1], [0.001, 0.005, 1])
+            self.open_loop_sys = control.tf([1], [0.001, 0.005, 1])
         else:
-            self.sys = control.tf([1], [0.001, 0.05, 1])
+            self.open_loop_sys = control.tf([1], [0.001, 0.05, 1])
 
-        # init rendering
-        plt.ion()
-        self.fig = plt.figure()
-        self.ax = self.fig.add_subplot(111)
-        self.first_flag = True
+        self.sim = TfSim(None, 10_000, 200, 100, 1.5)
 
-        # set model simulation params
-        self.model_sample_frequency = model_sample_frequency
-        self.controller_sample_frequency = controller_sample_frequency
-        self.model_steps_per_controller_value = int(self.model_sample_frequency / self.controller_sample_frequency)
-        self.simulation_time = simulation_time
-        self.n_sample_points = int(model_sample_frequency * simulation_time)
-        self.t = np.linspace(0, simulation_time, self.n_sample_points)
-        self.last_state = None
-        self.simulation_time_steps = 0
-        self.out = []
+        self.last_system_inputs = deque([0] * 3, maxlen=3)
+        self.last_system_outputs = deque([0] * 3, maxlen=3)
+        self.last_set_points = deque([0] * 3, maxlen=3)
+        self.w = []
+        self.w_sensor = []
         self.integrated_error = 0
-        self.last_action = 0
+        self.log = log
 
-        self.observation_space = gym.spaces.Box(low=np.array([-100, -1, -100, -100]), high=np.array([100, 1, 100, 100]),
-                                                shape=(4,),
+        self.episode_log = {"obs": {}, "rewards": {}, "action": {}, "function": {}}
+        self.log_all = []
+
+        self.observation_space = gym.spaces.Box(low=np.array([-100]*9), high=np.array([100]*9),
+                                                shape=(9,),
                                                 dtype=np.float32)
-        self.action_space = gym.spaces.Box(low=np.array([-0.99]), high=np.array([1]), shape=(1,),
+        self.action_space = gym.spaces.Box(low=np.array([-1]), high=np.array([1]), shape=(1,),
                                            dtype=np.float32)
 
-        # just for logging
-        self.actions_log = []
-        self.rewards_log = []
-        self.observations_log = []
-        self.tensorboard_log = {}
-        self.dones_log = []
-
     def reset(self, step_height=None, step_slope=None):
-        """
-        Important: the observation must be a numpy array; Return after every step with random step between 0, 2
-        :return: (np.array)
-        """
-        if not step_height:
-            step_height = random.uniform(-10, 10)
-        if not step_slope:
-            step_slope = random.uniform(0, 0.1)
-        u_before_step = [0] * int(0.5 * self.model_sample_frequency)
-        u_step = np.linspace(0, step_height, int(step_slope * self.model_sample_frequency)).tolist()
-        # u_step = []
-        u_after_step = [step_height] * int(self.n_sample_points - len(u_before_step) - len(u_step))
-        self.u = u_before_step + u_step + u_after_step
+        if len(self.episode_log["obs"]) != 0:
+            self.log_all.append(self.episode_log)
 
-        self.last_state = None
-        self.simulation_time_steps = 0
-        self.out = []
+        self.sim.reset()
+        self.w, self.w_sensor = self.set_w(step_height, step_slope)
         self.integrated_error = 0
-        self.last_action = 0
-        observation = [0, 0, 0, 0]
+        self.last_system_inputs = deque([0] * 3, maxlen=3)
+        self.last_system_outputs = deque([0] * 3, maxlen=3)
+        self.last_set_points = deque([0] * 3, maxlen=3)
 
-        return np.array(observation).astype(np.float32)
+        if self.log:
+            self.episode_log["obs"]["last_set_points"] = []
+            self.episode_log["obs"]["last_system_inputs"] = []
+            self.episode_log["obs"]["last_system_outputs"] = []
+            self.episode_log["obs"]["errors"] = []
+            self.episode_log["obs"]["set_point"] = []
+            self.episode_log["obs"]["system_input"] = []
+            self.episode_log["obs"]["system_output"] = []
+            self.episode_log["obs"]["set_point_vel"] = []
+            self.episode_log["obs"]["input_vel"] = []
+            self.episode_log["obs"]["outputs_vel"] = []
+            self.episode_log["rewards"]["summed"] = []
+            self.episode_log["rewards"]["pen_error"] = []
+            self.episode_log["rewards"]["pen_action"] = []
+            self.episode_log["rewards"]["pen_error_integrated"] = []
+            self.episode_log["action"]["value"] = []
+            self.episode_log["action"]["change"] = []
+            self.episode_log["function"]["w"] = None
+            self.episode_log["function"]["y"] = None
+
+
+        obs = self._create_obs(first=True)
+        return obs
+
+    def set_w(self, step_height=None, step_slope=None):
+        if step_height is None:
+            step_height = np.random.uniform(-10, 10)
+        if step_slope is None:
+            step_slope = np.random.uniform(0, 0.5)
+        w_before_step = [0] * int(0.5 * self.sim.model_freq)
+        w_step = np.linspace(0, step_height, int(step_slope * self.sim.model_freq)).tolist()
+        w_after_step = [step_height] * int(self.sim.n_sample_points - len(w_before_step) - len(w_step))
+        w = w_before_step + w_step + w_after_step
+        w_sensor = w[::self.sim.model_steps_per_senor_update]
+        return w, w_sensor
+
+    def _create_obs(self, first=False):
+        if first:
+            obs = list(self.last_set_points) + list(self.last_system_inputs) + list(self.last_system_outputs)
+        else:
+            obs = list(self.last_set_points) + list(self.last_system_inputs) + list(self.last_system_outputs)
+
+        if self.log:
+            self.episode_log["obs"]["last_set_points"].append(list(self.last_set_points))
+            self.episode_log["obs"]["last_system_inputs"].append(list(self.last_system_inputs))
+            self.episode_log["obs"]["last_system_outputs"].append(list(self.last_system_outputs))
+        return obs
+
+    def _create_reward(self):
+        y = np.array(list(self.last_system_outputs)[-self.sim.sensor_steps_per_controller_update:])
+        w = np.array(list(self.last_set_points)[-self.sim.sensor_steps_per_controller_update:])
+        e = np.mean(w - y)
+        self.integrated_error = self.integrated_error + e * (1/self.sim.model_steps_per_controller_update)
+        self.integrated_error = np.clip(self.integrated_error, -20, 20)
+
+        pen_error = np.abs(e)
+        pen_error = pen_error * 1
+        pen_action = np.abs(list(self.last_system_inputs)[-(self.sim.sensor_steps_per_controller_update+1)]
+                               - list(self.last_system_inputs)[-self.sim.sensor_steps_per_controller_update]) * 0.1
+        pen_integrated = np.abs(self.integrated_error) * 0
+        reward = -pen_error - pen_action - pen_integrated
+
+        if self.log:
+            self.episode_log["rewards"]["summed"].append(reward)
+            self.episode_log["rewards"]["pen_error"].append(-pen_error)
+            self.episode_log["rewards"]["pen_action"].append(-pen_action)
+            self.episode_log["rewards"]["pen_error_integrated"].append(-pen_integrated)
+
+        return reward
+
 
     def step(self, action):
-        controller_p = (action[0] + 1) * 50
+        controller_p = (action[0] + 1.0000001) * 50
         controller_p = np.clip(controller_p, 0, 100)
         # get p and i from action
         controller_i = 0
-        # controller_i = 0
-        # print(action)
+
+        if self.log:
+            self.episode_log["action"]["value"].append(controller_p)
+            self.episode_log["action"]["change"].append(controller_p - self.last_system_inputs[-1])
+
 
         # close loop with pi controller with given parameters
         pi_controller = control.tf([controller_p, controller_i], [1, 0])
-        open_loop = control.series(pi_controller, self.sys)
+        open_loop = control.series(pi_controller, self.open_loop_sys)
         closed_loop = control.feedback(open_loop, 1, -1)
+        # set sim sys
+        self.sim.sys = control.tf2ss(closed_loop)
 
-        # simulate one controller update step
-        start_step = self.simulation_time_steps
-        stop_step = self.simulation_time_steps + self.model_steps_per_controller_value
-        # try:
-        if self.last_state is None:
-            sim_time, out_step, self.last_state = control.forced_response(closed_loop,
-                                                                 self.t[start_step:stop_step+1],
-                                                                 self.u[start_step:stop_step+1],
-                                                                 return_x=True)
-        else:
-            sim_time, out_step, self.last_state = control.forced_response(closed_loop,
-                                                                 self.t[start_step:stop_step+1],
-                                                                 self.u[start_step:stop_step+1],
-                                                                 X0=self.last_state[:, -1],
-                                                                 return_x=True)
-        self.out = self.out + out_step.tolist()[:-1]
-        if stop_step >= len(self.t):
-            self.out = self.out + self.out[-1:]
+
+        self.sim.sim_one_step(u=self.w[self.sim.current_simulation_step:self.sim.current_simulation_step+self.sim.model_steps_per_controller_update+1])
+
+        for step in range(self.sim.sensor_steps_per_controller_update, 0, -1):
+            self.last_system_inputs.append(controller_p)
+            self.last_system_outputs.append(self.sim.sensor_out[self.sim.current_sensor_step - step])
+            self.last_set_points.append(self.w[self.sim.current_simulation_step - (step-1)*self.sim.model_steps_per_senor_update-1])
+
+        if self.sim.done:
             done = True
+            if self.log:
+                self.episode_log["function"]["w"] = self.w
+                self.episode_log["function"]["y"] = self.sim._sim_out
         else:
             done = False
 
-        # create observation
-        current_set_point = self.u[stop_step-1]
-        current_system_output = self.out[stop_step-1]
-        current_system_output_dot = (self.out[stop_step-1] - self.out[
-            stop_step-1 - self.model_steps_per_controller_value + 1]) / self.model_steps_per_controller_value * 100
-        current_system_input = action
-        obs = [current_set_point, current_system_input, current_system_output, current_system_output_dot]
+        obs = self._create_obs(first=False)
+        reward = self._create_reward()
 
-        # create reward
-        pen_error = np.mean(np.abs(np.array(self.out[start_step:stop_step-1]) - np.array(
-            self.u[start_step:stop_step-1])))  # mean error over last simulation step
-        pen_error = pen_error * 100
-        pen_action = controller_p * 0.1
-        pen_integrated = np.square(self.integrated_error) * 0
-        offset = 0
-        reward = offset - pen_error - pen_action - pen_integrated
-
-        # update simulation variables
-        self.simulation_time_steps += self.model_steps_per_controller_value
-        # self.last_action = system_input
-
-        # Optionally we can pass additional info, we are not using that for now
-        info = {}
-        # reward = 1 - reward
-        # just for logging
-        self.actions_log.append({"reference_value": controller_p,
-                                 "action": controller_p})
-        self.rewards_log.append({"offset": offset,
-                                 "reward": reward,
-                                 "pen_error": pen_error,
-                                 "pen_action": pen_action,
-                                 "pen_integrated": pen_integrated
-                                 })
-        self.observations_log.append(obs)
-        self.dones_log.append(done)
-        self.tensorboard_log = {"Obs/Error": obs,
-                                "Obs/Integrated_Error": self.integrated_error,
-                                "Action/P": controller_p,
-                                "Action/I": controller_i,
-                                "Reward": reward,
-                                "Done": 1 if done else 0}
-
-        return np.array(obs).astype(np.float32), reward, done, info
+        return np.array(obs), reward, done, {}
 
     def render(self, mode='console'):
-        x_points = [x for x in range(0, len(self.t), self.model_steps_per_controller_value)]
-        y_points = np.array(self.out)[x_points]
-        x_points = np.array(x_points) / self.model_sample_frequency
-        if self.first_flag:
-            self.ax.set_ylim(-0.1, 11)
+        ...
 
+    def create_eval_plot(self):
+        fig, ax = plt.subplots(2, 2, figsize=(20, 12))
+        timestamps = np.linspace(0, self.sim.simulation_time, int(self.sim.simulation_time * self.sim.controller_freq))
 
-            self.line1, = self.ax.plot(self.t, self.out, "r-")
-            self.line2, = self.ax.plot(self.t, self.u)
-            self.line3 = self.ax.scatter(x_points, y_points)
-            self.ax.grid()
-            self.first_flag = False
-        else:
-            self.line1.set_ydata(self.out)
-            self.line2.set_ydata(self.u)
-            self.line3.set_offsets(np.array([x_points, y_points]).T)
-            self.fig.canvas.draw()
-            self.fig.canvas.flush_events()
+        ax[0][0].set_title("Obs")
+        for key, value in self.episode_log["obs"].items():
+            if len(value) > 0:
+                ax[0][0].plot(timestamps, value[:-1], label=key)  # verschoben zum Reset; eine Obs mehr als Reward!
+        ax[0][0].grid()
+        ax[0][0].legend()
+
+        ax[1][0].set_title("Rewards")
+        for key, value in self.episode_log["rewards"].items():
+            if len(value) > 0:
+                ax[1][0].plot(timestamps, value, label=key)
+        ax[1][0].grid()
+        ax[1][0].legend()
+
+        ax[0][1].set_title("Action")
+        for key, value in self.episode_log["action"].items():
+            if len(value) > 0:
+                ax[0][1].plot(timestamps, value, label=key)
+        ax[0][1].grid()
+        ax[0][1].legend()
+
+        ax[1][1].set_title("Function")
+        for key, value in self.episode_log["function"].items():
+            if len(value) > 0:
+                ax[1][1].plot(self.sim.t, value, label=key)
+        ax[1][1].grid()
+        ax[1][1].legend()
+
+        fig.tight_layout()
+        return fig
+
