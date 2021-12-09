5ec15a51dff96b505341f2f1c2e8b4034a6684cb
Julius <julius.brandl97@gmail.com>
2021-12-09 11:14:11 +0100

    use different net sizes and activation functions

--- runner.py
+++ runner.py
@@ -40,46 +40,55 @@ def linear_schedule(initial_value: float=1e-3) -> Callable[[float], float]:
     return func
 
 
-# for action_noise in [0.3, 0.2, 0.1, 0.05, 0.02, 0.01, 0.005, 0.003, 0.001, 0.0001, 0]:
-for action_noise in [0.001]:
-    RUN_NAME = f"obs_with_vel_diff_action_noise_{action_noise}"
+# ACTOR_NET = [10, 10]
+# CRITIC_NET = [200, 200]
+# AF = th.nn.Sigmoid
+# AF_STRING = "Sigmoid"
 
-    # create DmsSim Gym Env
-    env = DirectControllerPT2()
-    env = Monitor(env)
 
-    online_eval_env = DirectControllerPT2(log=True)
-    online_eval_env = Monitor(online_eval_env)
-    # env = VecCheckNan(env)
+for actor_net in [[2, 2], [3, 3], [5, 5], [50, 50]]:
+    for critic_net in [[100, 100], [200, 200], [300, 300], [500, 500]]:
+        for af, af_name in zip([th.nn.Sigmoid, th.nn.Tanh, th.nn.ReLU], ["Sigmoid", "TanH", "ReLU"]):
 
-    # env = DummyVecEnv([lambda: DirectControllerPT2()])
-    # env = VecCheckNan(env, raise_exception=True)
+            # for action_noise in [0.3, 0.2, 0.1, 0.05, 0.02, 0.01, 0.005, 0.003, 0.001, 0.0001, 0]:
+            RUN_NAME = f"obs_with_vel_diff-actor_net_{actor_net}_critic_net_{critic_net}_activation_fn_{af_name}_test"
 
-    # create action noise
-    n_actions = env.action_space.shape[-1]
-    action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=float(0.01) * np.ones(n_actions))
+            # create DmsSim Gym Env
+            env = DirectControllerPT2()
+            env = Monitor(env)
 
-    # create eval callback
-    eval_callback = CustomEvalCallback(online_eval_env, eval_freq=1500, deterministic=True)
+            online_eval_env = DirectControllerPT2(log=True)
+            online_eval_env = Monitor(online_eval_env)
+            # env = VecCheckNan(env)
 
-    # create callback list
-    callbacks = CallbackList([eval_callback])
+            # env = DummyVecEnv([lambda: DirectControllerPT2()])
+            # env = VecCheckNan(env, raise_exception=True)
 
-    # # use DDPG and create a tensorboard
-    # # start tensorboard server with tensorboard --logdir ./dmsSim_ddpg_tensorboard/
-    # policy_kwargs = dict(activation_fn=th.nn.Tanh)
-    # policy_kwargs = dict(net_arch=dict(pi=[64, 64], qf=[200, 200]))
+            # create action noise
+            n_actions = env.action_space.shape[-1]
+            action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=float(0.1) * np.ones(n_actions))
 
-    model = DDPG(MlpPolicy,
-                 env,
-                 learning_starts=3000,
-                 verbose=2,
-                 action_noise=action_noise,
-                 tensorboard_log="./eval_action_noise/",
-                 #policy_kwargs=policy_kwargs,
-                 )
-    model.learn(total_timesteps=50_000, tb_log_name=RUN_NAME, callback=callbacks)
-    utils.eval(DirectControllerPT2(log=True), model, folder_name=RUN_NAME)
-    # #
-    # # # save model if you want to
-    model.save(f"eval\\{RUN_NAME}\\model")
+            # create eval callback
+            eval_callback = CustomEvalCallback(online_eval_env, eval_freq=5000, deterministic=True)
+
+            # create callback list
+            callbacks = CallbackList([eval_callback])
+
+            # # use DDPG and create a tensorboard
+            # # start tensorboard server with tensorboard --logdir ./dmsSim_ddpg_tensorboard/
+            # policy_kwargs = dict(activation_fn=th.nn.Tanh)
+            policy_kwargs = dict(net_arch=dict(pi=actor_net, qf=critic_net), activation_fn=af)
+
+            model = DDPG(MlpPolicy,
+                         env,
+                         learning_starts=3000,
+                         verbose=2,
+                         action_noise=action_noise,
+                         tensorboard_log="./net_params_discrete_reward/",
+                         policy_kwargs=policy_kwargs,
+                         )
+            model.learn(total_timesteps=100_000, tb_log_name=f"actor_net{actor_net}_critic_net{critic_net}_af_{af_name}", callback=callbacks)
+            utils.eval(DirectControllerPT2(log=True), model, folder_name=RUN_NAME)
+            # #
+            # # # save model if you want to
+            model.save(f"eval\\{RUN_NAME}\\model")
