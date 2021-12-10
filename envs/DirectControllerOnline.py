9894b2a9dd666774fce6be9f120e468a137fef36
jubra97 <julius.brandl97@gmail.com>
2021-12-10 18:00:08 +0100

    read from sps with buffers; write u

--- envs/DirectControllerOnline.py
+++ envs/DirectControllerOnline.py
@@ -1,110 +1,101 @@
 import pyads
 import time
 import numpy as np
-
-# pseudo step:
-# def step(action):
-    # holen der buffer für obs und reward
-    # setzzen der neuen action
-
-timesteps = []
-last_timestamp = 0
-
-# connect to plc and open connection
-plc = pyads.Connection('192.168.10.200.1.1', 352)
-with plc:
-    vars = plc.get_all_symbols()
-    for var in vars:
-        print(var)
-
-    dms_buffer_handle = plc.get_handle("Object3 (RL_Learn_DirectControl).Output.ads_buffer")
-    start_time = time.time()
-    while True:
-        if time.time() - start_time > 0.01:
-            start_time = time.time()
-            print("______")
-            timestamp, setpoint, dms = plc.read_by_name("", pyads.PLCTYPE_LREAL * 20 * 3, handle=dms_buffer_handle)
-            # for value in timestamp:
-            #     print(value)
-
-            new_timesamps = np.array(timestamp)
-            diff_timetsamps = new_timesamps - last_timestamp
-            print(new_timesamps)
-            print(last_timestamp)
-            print(diff_timetsamps)
-
-            try:
-                inx = diff_timetsamps.tolist().index(0)+1
-            except ValueError:
-                inx = 0
-
-            timesteps = timesteps + new_timesamps[inx:].tolist()
-            last_timestamp = timesteps[-1:]
+from ctypes import sizeof, Structure
+import matplotlib.pyplot as plt
+import threading
 
 
-            if np.isclose(np.diff(timesteps), 0.001).all():
-                print("GEIL")
+BUFFER_SIZE = 20
+
+
+class AdsBuffer(Structure):
+
+    _fields_ = [
+
+        ("t", pyads.PLCTYPE_LREAL * BUFFER_SIZE),
+        ("w", pyads.PLCTYPE_LREAL * BUFFER_SIZE),
+        ("u", pyads.PLCTYPE_LREAL * BUFFER_SIZE),
+        ("y", pyads.PLCTYPE_LREAL * BUFFER_SIZE)
+
+    ]
+
+
+class DirectControllerOnline:
+    def __init__(self):
+        self.last_timestamps = []
+        self.last_timestamp = 0
+
+        self.ads_buffer_mutex = threading.Lock()
+        self.last_t = []
+        self.last_w = []
+        self.last_y = []
+        self.last_u = []
+
+        self.plc = pyads.Connection('192.168.10.200.1.1', 352)
+        self.plc.open()
+        # symbols = self.plc.get_all_symbols()
+        # for sym in symbols:
+        #     print(sym)
+
+        self.ads_buffer_sps = self.plc.get_symbol("Object3 (RL_Learn_DirectControl).Output.ads_buffer", array_size=20)
+        attr = pyads.NotificationAttrib(length=sizeof(AdsBuffer), trans_mode=pyads.ADSTRANS_SERVERCYCLE, max_delay=1.0, cycle_time=5.0)
+        self.ads_buffer_sps.add_device_notification(self.update_obs, attr)
+
+        self.input_u = self.plc.get_symbol("Object3 (RL_Learn_DirectControl).Input.goal_torque")
+
+    def update_obs(self, notification, data):
+        _handle, _datetime, value = self.plc.parse_notification(
+            notification, self.ads_buffer_sps.plc_type
+        )
+
+        t = value[:BUFFER_SIZE]
+        w = value[BUFFER_SIZE:BUFFER_SIZE*2]
+        u = value[BUFFER_SIZE*2:BUFFER_SIZE*3]
+        y = value[BUFFER_SIZE*3:BUFFER_SIZE*4]
+
+        with self.ads_buffer_mutex:
+            if not self.last_t:
+                self.last_t = t
+                self.last_w = w
+                self.last_u = u
+                self.last_y = y
+
             else:
-                print(np.diff(timesteps))
-                print(timesteps)
-                raise ValueError("STOP")
+                # check witch data is new
+                is_new_timestamp = (np.array(t) - self.last_t[-1]) > 0
+                if is_new_timestamp.all():
+                    raise ValueError("Lost Step")
+                index_first_new_value = is_new_timestamp.tolist().index(1)
+                 # add new data to lists
+                self.last_t += t[index_first_new_value:]
+                self.last_w += w[index_first_new_value:]
+                self.last_u += u[index_first_new_value:]
+                self.last_y += y[index_first_new_value:]
+
+    def set_u(self, u):
+        self.input_u.write(u)
 
 
+if __name__ == "__main__":
+    env = DirectControllerOnline()
 
+    for i in range(1):
+        print(i)
+        time.sleep(60)
 
-        time.sleep(0.00001)
+    with env.ads_buffer_mutex:
+        plt.plot(env.last_t, env.last_w)
+        plt.plot(env.last_t, env.last_u)
+        plt.plot(env.last_t, env.last_y)
+        plt.show()
 
+    with env.ads_buffer_mutex:
+        plt.plot(env.last_t)
+        plt.show()
 
-
-
-
-
-
-
-    #
-    # start_time = time.time()
-    # print(start_time-start_time)
-    # plc.write_by_name("Object3 (ads_test).Input.timestamp_in", start_time)
-    # print(time.time()-start_time)
-    # return_time = plc.read_by_name("Object3 (ads_test).Input.timestamp_in")
-    # print(time.time()-start_time)
-    #
-    # var_handle = plc.get_handle("Object3 (ads_test).Input.timestamp_in")
-    #
-    # times = []
-    # for _ in range(1000):
-    #     start_time = time.time()
-    #     plc.write_by_name("", start_time, pyads.PLCTYPE_LREAL, handle=var_handle)
-    #     return_time = plc.read_by_name("", pyads.PLCTYPE_LREAL, handle=var_handle)
-    #     times.append(time.time()-start_time)
-    #
-    # print(np.mean(times))
-    # print(max(times))
-    # print(min(times))
-    # plc.release_handle(var_handle)
-    # timestamps = []
-    # @plc.notification(pyads.PLCTYPE_LREAL)
-    # def callback(handle, name, timestamp, value):
-    #     # print(
-    #     #     '{1}: received new notitifiction for variable "{0}", value: {2}'
-    #     #         .format(name, timestamp, value)
-    #     # )
-    #     timestamps.append(timestamp)
-    #
-    #
-    # notification_handle, var_handle = plc.add_device_notification("Object3 (ads_test).Input.timestamp_in", pyads.NotificationAttrib(8),
-    #                             callback)
-    #
-    # for i in range(50000):
-    #     plc.write_by_name("", i, pyads.PLCTYPE_LREAL, handle=var_handle)
-    #
-    # time.sleep(1)
-    # timestamps = np.array(timestamps)
-    # diffs = (timestamps[1:] - timestamps[:-1])
-    # print(diffs)
-    # print(np.mean(diffs))
-    # print(max(diffs))
-    # print(min(diffs))
-    #
-    # # plc.release_handle(notification_handle)
-    # plc.release_handle(var_handle)
+    with env.ads_buffer_mutex:
+        plt.plot(env.last_t, env.last_w)
+        plt.plot(env.last_t, env.last_u)
+        plt.plot(env.last_t, env.last_y)
+        plt.show()
