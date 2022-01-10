68fbd63a764b427e2ee83aa41a239669e6c381c6
Julius <julius.brandl97@gmail.com>
2022-01-10 11:45:33 +0100

    add docstrings remove unnecessary files

--- envs/DirectControllerv2.py
+++ envs/DirectControllerv2.py
@@ -1,55 +1,100 @@
-import time
+import json
+import pathlib
 from collections import deque
 
 import control
 import gym
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.fft import fft, fftfreq
+
 from envs.TfSim import TfSim
 
 
 class DirectControllerPT2(gym.Env):
 
-    def __init__(self, oscillating=False, log=False):
+    def __init__(self, oscillating=False, log=False, reward_function="discrete", oscillation_pen_gain=1,
+                 oscillation_pen_fun=np.sqrt, error_pen_fun=None):
+        """
+        Create a gym environment to directly control the actuating value (u) of a system.
+        :param oscillating: If True a
+        :param log: Log the simulation outcomes.
+        """
         super(DirectControllerPT2, self).__init__()
         if oscillating:
             sys = control.tf([1], [0.001, 0.005, 1])
         else:
             sys = control.tf([1], [0.001, 0.05, 1])
 
-        self.sim = TfSim(sys, 10_000, 200, 100, 1.5)
+        # input_lag = control.tf([1], [0.0001, 0.01, 1])
+        # sys = control.series(input_lag, sys)
 
+        self.sim = TfSim(sys, 10_000, 200, 100, 1.5)  # create simulation object with an arbitrary tf.
+
+        if reward_function == "discrete":
+            self.reward_function = self._create_reward_discrete
+        elif reward_function == "normal":
+            self.reward_function = self._create_reward
+        else:
+            raise ValueError("No corresponding reward function could be found. If you want to add your own reward"
+                             "function add the function to this if else block.")
+        self.oscillation_pen_gain = oscillation_pen_gain
+        self.oscillation_pen_fun = oscillation_pen_fun
+        self.error_pen_fun = error_pen_fun
+        # create fifo lists for newest simulation outcomes.
         self.last_system_inputs = deque([0] * 3, maxlen=3)
         self.last_system_outputs = deque([0] * 3, maxlen=3)
         self.last_set_points = deque([0] * 3, maxlen=3)
+
         self.w = []
-        self.w_sensor = []
         self.integrated_error = 0
-        self.abs_integrated_error = 0
+
+        # create variables for logging
+        self.episode_log = {"obs": {}, "rewards": {}, "action": {}, "function": {}}
         self.log = log
         self.n_episodes = 0
-
-        self.episode_log = {"obs": {}, "rewards": {}, "action": {}, "function": {}}
         self.log_all = []
 
-        self.observation_space = gym.spaces.Box(low=np.array([-100]*7), high=np.array([100]*7),
+        # define size of observation and action space
+        self.observation_space = gym.spaces.Box(low=np.array([-100] * 7, dtype=np.float32),
+                                                high=np.array([100] * 7, dtype=np.float32),
                                                 shape=(7,),
                                                 dtype=np.float32)
-        self.action_space = gym.spaces.Box(low=np.array([-1]), high=np.array([1]), shape=(1,),
+        self.action_space = gym.spaces.Box(low=np.array([-1], dtype=np.float32),
+                                           high=np.array([1], dtype=np.float32),
+                                           shape=(1,),
                                            dtype=np.float32)
 
-    def reset(self, step_height=None, step_slope=None):
+    def reset(self, step_height=None, step_slope=None, custom_w=None):
+        """
+        Reset the environment. Called before every start of a new episode. If no custom reference value (custom_w) is
+        given a ramp (or step) is used.
+        :param step_height: Height of step/ramp.
+        :param step_slope: Slope of Step. If slope is 0 a step is generated.
+        :param custom_w:
+        :return:
+        """
+
+        # add log of last episode to a list of logs of all episodes
         if self.n_episodes != 0:
             self.log_all.append(self.episode_log)
         self.n_episodes += 1
 
+        # reset simulation
         self.sim.reset()
-        self.w, self.w_sensor = self.set_w(step_height, step_slope)
-        self.integrated_error = 0
-        self.abs_integrated_error = 0
+
+        # create reference value (w)
+        if custom_w is not None:
+            assert self.sim.n_sample_points == len(custom_w), "Simulation and input length must not differ"
+            self.w = custom_w
+        else:
+            self.w = self.set_w(step_height, step_slope)
+
+        # reset stuff that is only valid in the same episode
         self.last_system_inputs = deque([0] * 3, maxlen=3)
         self.last_system_outputs = deque([0] * 3, maxlen=3)
         self.last_set_points = deque([0] * 3, maxlen=3)
+        self.integrated_error = 0
 
         if self.log:
             self.episode_log["obs"]["last_set_points"] = []
@@ -72,11 +117,17 @@ class DirectControllerPT2(gym.Env):
             self.episode_log["function"]["w"] = None
             self.episode_log["function"]["y"] = None
 
-
+        # create start observation of new episode
         obs = self._create_obs_with_vel(first=True)
         return obs
 
     def set_w(self, step_height=None, step_slope=None):
+        """
+        Create reference value (w) as ramp or step
+        :param step_height: Height of ramp/ step
+        :param step_slope: Slope of ramp; if 0 a step is generated
+        :return:
+        """
         if step_height is None:
             step_height = np.random.uniform(-10, 10)
         if step_slope is None:
@@ -85,22 +136,23 @@ class DirectControllerPT2(gym.Env):
         w_step = np.linspace(0, step_height, int(step_slope * self.sim.model_freq)).tolist()
         w_after_step = [step_height] * int(self.sim.n_sample_points - len(w_before_step) - len(w_step))
         w = w_before_step + w_step + w_after_step
-        w_sensor = w[::self.sim.model_steps_per_senor_update]
-        return w, w_sensor
+        return w
 
     def _create_obs_with_vel(self, first=False):
         set_points = np.array(list(self.last_set_points))
         system_outputs = np.array(list(self.last_system_outputs))
         system_inputs = np.array(list(self.last_system_inputs))
 
-        outputs_vel = (system_outputs[-2] - system_outputs[-1]) * 1/self.sim.sensor_steps_per_controller_update
-        input_vel = (system_inputs[-3] - system_inputs[-1]) * 1/self.sim.model_steps_per_controller_update
-        set_point_vel = (set_points[-2] - set_points[-1]) * 1/self.sim.sensor_steps_per_controller_update
+        outputs_vel = (system_outputs[-2] - system_outputs[-1]) * 1 / self.sim.sensor_steps_per_controller_update
+        input_vel = (system_inputs[-3] - system_inputs[-1]) * 1 / self.sim.model_steps_per_controller_update
+        set_point_vel = (set_points[-2] - set_points[-1]) * 1 / self.sim.sensor_steps_per_controller_update
 
         if first:
-            obs = [set_points[-1], system_inputs[-1], system_outputs[-1], set_point_vel, input_vel, outputs_vel, self.integrated_error]
+            obs = [set_points[-1], system_inputs[-1], system_outputs[-1], set_point_vel, input_vel, outputs_vel,
+                   self.integrated_error]
         else:
-            obs = [set_points[-1], system_inputs[-1], system_outputs[-1], set_point_vel, input_vel, outputs_vel, self.integrated_error]
+            obs = [set_points[-1], system_inputs[-1], system_outputs[-1], set_point_vel, input_vel, outputs_vel,
+                   self.integrated_error]
 
         if self.log:
             self.episode_log["obs"]["set_point"].append(set_points[-1])
@@ -141,88 +193,118 @@ class DirectControllerPT2(gym.Env):
         return obs
 
     def _create_reward(self):
+        """
+        Create reward as a combination of the current error e between y and w and the change of of the action (to
+        smaller the oscillation in u).
+        :return: reward
+        """
+        # get latest system attributes and calculate error/ integrated error
         y = np.array(list(self.last_system_outputs)[-self.sim.sensor_steps_per_controller_update:])
         w = np.array(list(self.last_set_points)[-self.sim.sensor_steps_per_controller_update:])
         e = np.mean(w - y)
-        self.integrated_error = self.integrated_error + e * (1/self.sim.model_steps_per_controller_update)
+        self.integrated_error = self.integrated_error + e * (1 / self.sim.model_steps_per_controller_update)
         self.integrated_error = np.clip(self.integrated_error, -0.3, 0.3)
-        self.abs_integrated_error = self.abs_integrated_error + abs(e) * (1/self.sim.model_steps_per_controller_update)
-        self.abs_integrated_error = np.clip(self.abs_integrated_error, 0, 20)
 
+        # calculate action change
+        action_change = self.last_system_inputs[-(self.sim.sensor_steps_per_controller_update + 1)] \
+                        - self.last_system_inputs[-self.sim.sensor_steps_per_controller_update]
 
-        pen_error = np.abs(e)
-        pen_error = np.sqrt(pen_error) * 15
-        pen_action = np.abs(list(self.last_system_inputs)[-(self.sim.sensor_steps_per_controller_update+1)]
-                               - list(self.last_system_inputs)[-self.sim.sensor_steps_per_controller_update])
-        pen_action = np.sqrt(pen_action) * 1
-        pen_integrated = np.square(self.integrated_error) * 0
+        pen_error = np.square(e)
+        pen_action = np.square(action_change) * 0.01
+        # pen_integrated = np.square(self.integrated_error) * 0
 
-        reward = pen_error + pen_action + pen_integrated
-        reward = -reward
+        reward = 0
+        reward -= pen_error
+        reward -= pen_action
 
         if self.log:
             self.episode_log["rewards"]["summed"].append(reward)
             self.episode_log["rewards"]["pen_error"].append(-pen_error)
             self.episode_log["rewards"]["pen_action"].append(-pen_action)
-            self.episode_log["rewards"]["pen_error_integrated"].append(-pen_integrated)
+            # self.episode_log["rewards"]["pen_error_integrated"].append(-pen_integrated)
 
         return reward
 
     def _create_reward_discrete(self):
+        # get latest system attributes and calculate error/ integrated error
         y = np.array(list(self.last_system_outputs)[-self.sim.sensor_steps_per_controller_update:])
         w = np.array(list(self.last_set_points)[-self.sim.sensor_steps_per_controller_update:])
         e = np.mean(w - y)
-        self.integrated_error = self.integrated_error + e * (1/self.sim.model_steps_per_controller_update)
-        self.integrated_error = np.clip(self.integrated_error, -20, 20)
-        self.abs_integrated_error = self.abs_integrated_error + abs(e) * (1/self.sim.model_steps_per_controller_update)
-        self.abs_integrated_error = np.clip(self.abs_integrated_error, 0, 20)
+        self.integrated_error = self.integrated_error + e * (1 / self.sim.model_steps_per_controller_update)
+        self.integrated_error = np.clip(self.integrated_error, -0.3, 0.3)
 
-        pen_action = np.abs(list(self.last_system_inputs)[-(self.sim.sensor_steps_per_controller_update+1)]
-                               - list(self.last_system_inputs)[-self.sim.sensor_steps_per_controller_update]) * 2
+        # calculate action change
+        action_change = self.last_system_inputs[-(self.sim.sensor_steps_per_controller_update + 1)] \
+                        - self.last_system_inputs[-self.sim.sensor_steps_per_controller_update]
 
-        pen_action = np.clip(pen_action, 0, 4)
+        abs_error = abs(e)
+        # pen_error = np.sqrt(abs(e))
+        # pen_action = np.sqrt(abs(action_change)) * 0.1
+        # pen_action = np.clip(pen_action, 0, 10)
 
-        abs_error = np.abs(e)
+        if self.error_pen_fun:
+            pen_error = self.error_pen_fun(abs(e))
+        else:
+            pen_error = abs(e)
+
+        if self.oscillation_pen_fun:
+            pen_action = self.oscillation_pen_fun(abs(action_change)) * self.oscillation_pen_gain
+        else:
+            pen_action = abs(action_change) * self.oscillation_pen_gain
+
         reward = 0
         if abs_error < 5:
-            reward += 1
+            reward += 0.5
         if abs_error < 1:
-            reward += 2
+            reward += 1
         if abs_error < 0.1:
-            reward += 5
+            reward += 2
         if abs_error < 0.05:
-            reward += 10
+            reward += 3
         if abs_error < 0.02:
-            reward += 15
+            reward += 5
         if abs_error < 0.005:
-            reward += 20
-        reward -= pen_action + (abs_error * 0.2)
+            reward += 10
+
+        reward -= pen_error
+        reward -= pen_action
 
         if self.log:
             self.episode_log["rewards"]["summed"].append(reward)
             self.episode_log["rewards"]["pen_action"].append(-pen_action)
-            self.episode_log["rewards"]["pen_error"].append(-abs_error * 0.2)
+            self.episode_log["rewards"]["pen_error"].append(-pen_error)
         return reward
 
-
     def step(self, action):
-        # action und obs oder last action und obs? Schaue ich sonst in die Zukunft?
-        system_input = np.clip(action[0] * 20, -20, 20)
+        """
+        Step the environment for one update step of the action. Collect sensor values of the simulation and compute
+        reward and next observation from them.
+        :param action: Next u for simulation.
+        :return:
+        """
+        # scale normalized action to fit to system values.
+        system_input = np.clip(action[0] * 20, -20, 20)  # u
 
-        # system_input = round(system_input, 1)
+        # create static input for every simulation step until next update of u.
         system_input_trajectory = [system_input] * (self.sim.model_steps_per_controller_update + 1)
 
         if self.log:
             self.episode_log["action"]["value"].append(system_input)
             self.episode_log["action"]["change"].append(system_input - self.last_system_inputs[-1])
 
+        # simulate system until next update of u.
         self.sim.sim_one_step(u=system_input_trajectory, add_noise=True)
 
-        # if self.sim.current_simulation_step != 0:
+        # update fifo lists with newest values. In simulation a simulation sample time, a sensor sample time, and a u
+        # update sample time is used. A smaller simulation sample time used for more precise simulation results.
+        # A sensor sample time is used to simulate that a potential sensor is slower than the system dynamics.
+        # The u update sample time is used that a potential actor / calculation of u is even slower than the sensor.
+        # For the fifo lists the newest sensor values are used.
         for step in range(self.sim.sensor_steps_per_controller_update, 0, -1):
             self.last_system_inputs.append(self.sim.u_sensor[self.sim.current_sensor_step - step])
             self.last_system_outputs.append(self.sim.sensor_out[self.sim.current_sensor_step - step])
-            self.last_set_points.append(self.w[self.sim.current_simulation_step - (step-1)*self.sim.model_steps_per_senor_update-1])
+            self.last_set_points.append(
+                self.w[self.sim.current_simulation_step - (step - 1) * self.sim.model_steps_per_senor_update - 1])
 
         if self.sim.done:
             done = True
@@ -232,16 +314,44 @@ class DirectControllerPT2(gym.Env):
         else:
             done = False
 
+        # create obs and reward
         obs = self._create_obs_with_vel(first=False)
-        reward = self._create_reward_discrete()
+        reward = self.reward_function()
 
         return np.array(obs), reward, done, {}
 
     def render(self, mode="human"):
+        """
+        Must be here to implement abstract methods of super class.
+        :param mode:
+        :return:
+        """
         ...
 
+    def eval_fft(self):
+        """
+        Calculate fft of the action signal for one episode. Also compute the action smoothness value in
+        https: // arxiv.org / pdf / 2012.06644.pdf.
+        :return:
+        """
+        N = 150
+        T = 1 / 100
+
+        actions = self.episode_log["action"]["value"]
+        actions = actions - np.mean(actions)
+        actions_fft = fft(actions)
+        actions_fftfreq = fftfreq(N, T)[:N // 2]
+
+        # https: // arxiv.org / pdf / 2012.06644.pdf Smoothness Measurement
+        sm = (2 / actions_fftfreq.shape[0]) * np.sum(actions_fftfreq * 2 / N * np.abs(actions_fft[0:N // 2]))
+        return actions_fft, actions_fftfreq, N, sm
+
     def create_eval_plot(self):
-        fig, ax = plt.subplots(2, 2, figsize=(20, 12))
+        """
+        Crate a plot for tensorboard while training and afterwards for evaluation.
+        :return:
+        """
+        fig, ax = plt.subplots(2, 3, figsize=(20, 12))
         timestamps = np.linspace(0, self.sim.simulation_time, int(self.sim.simulation_time * self.sim.controller_freq))
 
         ax[0][0].set_title("Obs")
@@ -272,5 +382,57 @@ class DirectControllerPT2(gym.Env):
         ax[1][1].grid()
         ax[1][1].legend()
 
+        ax[1][2].set_title("FFT")
+        actions_fft, actions_fftfreq, N, sm = self.eval_fft()
+        ax[1][2].plot(actions_fftfreq, 2 / N * np.abs(actions_fft[0:N // 2]))
+        ax[1][2].text(0.5, 0.9, f"Smoothness: {sm}", transform=ax[1][2].transAxes)
+        ax[1][2].grid()
+
         fig.tight_layout()
         return fig
+
+    def eval(self, model, folder_name):
+        """
+        Run an evaluation with different steps and ramps. Create a plot for every run and save it. Also save a json file
+        with some statistics of a run.
+        :param model: Model to be used for action prediction.
+        :param folder_name: Folder to save evaluation in.
+        """
+        # steps = range(-10, 11)
+        # slopes = np.linspace(0, 0.5, 3)
+        steps = [0]
+        slopes = [0, 0.1]
+        i = 1
+        pathlib.Path(f"eval\\{folder_name}").mkdir(exist_ok=True)
+        rewards = []
+        rmse = []
+        sms = []
+        extra_info = {}
+        for step in steps:
+            for slope in slopes:
+                # slope = slope * 0.1
+                actions = []
+                # create env
+                done = False
+                obs = self.reset(step, slope)
+                while not done:
+                    action, _ = model.predict(obs)
+                    obs, reward, done, info = self.step(action)
+                    rewards.append(reward)
+                    actions.append(action)
+                _, _, _, smoothness = self.eval_fft()
+                sms.append(smoothness)
+                fig = self.create_eval_plot()
+                plt.savefig(f"eval\\{folder_name}\\{i}_{step}_{slope}.png")
+                plt.close()
+                i += 1
+                rmse_episode = np.sqrt(np.square(np.array(self.w) - np.array(self.sim._sim_out)))
+                rmse.append(rmse_episode)
+        mean_episode_reward = np.sum(rewards) / self.n_episodes
+        extra_info["mean_episode_reward"] = mean_episode_reward
+        extra_info["rmse"] = np.mean(rmse)
+        extra_info["smoothness"] = np.mean(sms)
+
+        with open(f"eval\\{folder_name}\\extra_info.json", 'w+') as f:
+            json.dump(extra_info, f)
+        print(f"Eval Info: RMSE: {np.mean(rmse)} --- Smoothness: {np.mean(sms)}")
--- runner.py
+++ runner.py
@@ -1,95 +1,55 @@
-import time
-
 import numpy as np
-import json
-# import matplotlib
-from typing import Callable
-from stable_baselines3.ddpg.policies import MlpPolicy
-from stable_baselines3.common.noise import NormalActionNoise
-from stable_baselines3 import DDPG, TD3
-from stable_baselines3.common.callbacks import CallbackList
-# from envs.DirectControllerv2 import DirectControllerPT2
-from envs.PIAdaptivePT2 import PIAdaptivePT2
-from tensorboard_logger import TensorboardCallback
-import matplotlib.pyplot as plt
-from CustomEvalCallback import CustomEvalCallback
-from stable_baselines3.common.monitor import Monitor
-from stable_baselines3.common.vec_env import VecCheckNan, DummyVecEnv, SubprocVecEnv
-from stable_baselines3.common.env_util import make_vec_env
 import torch as th
-import utils
-# from envs.DirectControllerOnline import DirectControllerOnline
+from stable_baselines3 import DDPG
+from stable_baselines3.common.callbacks import CallbackList
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.noise import NormalActionNoise
+from stable_baselines3.common.vec_env import SubprocVecEnv
+from stable_baselines3.ddpg.policies import MlpPolicy
 
-def linear_schedule(initial_value: float=1e-3) -> Callable[[float], float]:
-    """
-    Linear learning rate schedule.
+from CustomEvalCallback import CustomEvalCallback
+from envs.DirectControllerv2 import DirectControllerPT2
 
-    :param initial_value: Initial learning rate.
-    :return: schedule that computes
-      current learning rate depending on remaining progress
-    """
+actor_net = [20, 20]
+critic_net = [200, 200]
+af = th.nn.Tanh
 
-    def func(progress_remaining: float) -> float:
-        """
-        Progress will decrease from 1 (beginning) to 0.
-
-        :param progress_remaining:
-        :return: current learning rate
-        """
-        return progress_remaining * initial_value
-
-    return func
-
-
-# ACTOR_NET = [10, 10]
-# CRITIC_NET = [200, 200]
-# AF = th.nn.Sigmoid
-# AF_STRING = "Sigmoid"
+# TODO: Which observation should be used?
 
 if __name__ == "__main__":
-    for actor_net in [[20, 20]]:
-        for critic_net in [[200, 200]]:
-            for af, af_name in zip([th.nn.Tanh], ["TanH"]):
+    RUN_NAME = f"Run"
 
-                # for action_noise in [0.3, 0.2, 0.1, 0.05, 0.02, 0.01, 0.005, 0.003, 0.001, 0.0001, 0]:
-                RUN_NAME = f"obs_with_vel_diff-actor_net_{actor_net}_critic_net_{critic_net}_activation_fn_{af_name}_test"
+    env = make_vec_env(DirectControllerPT2, 2, vec_env_cls=SubprocVecEnv)  # create learning env
 
-                # create DmsSim Gym Env
-                # env = PIAdaptivePT2()
-                # env = Monitor(env)
+    # create action noise
+    n_actions = env.action_space.shape[-1]
+    action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=float(0.1) * np.ones(n_actions))
 
-                env = make_vec_env(PIAdaptivePT2, 2, vec_env_cls=SubprocVecEnv)
+    # create eval callback
+    online_eval_env = DirectControllerPT2(log=True)  # create eval env
+    online_eval_env = Monitor(online_eval_env)
+    eval_callback = CustomEvalCallback(online_eval_env, eval_freq=1500, deterministic=True)
 
-                online_eval_env = PIAdaptivePT2(log=True)
-                online_eval_env = Monitor(online_eval_env)
+    # create callback list
+    callbacks = CallbackList([eval_callback])
 
-                # create action noise
-                n_actions = env.action_space.shape[-1]
-                action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=float(0.1) * np.ones(n_actions))
+    # # use DDPG and create a tensorboard
+    # # start tensorboard server with tensorboard --logdir ./{tensorboard_log}/
+    policy_kwargs = dict(net_arch=dict(pi=actor_net, qf=critic_net), activation_fn=af)
 
-                # create eval callback
-                eval_callback = CustomEvalCallback(online_eval_env, eval_freq=5000, deterministic=True)
-
-                # create callback list
-                callbacks = CallbackList([eval_callback])
-
-                # # use DDPG and create a tensorboard
-                # # start tensorboard server with tensorboard --logdir ./dmsSim_ddpg_tensorboard/
-                # policy_kwargs = dict(activation_fn=th.nn.Tanh)
-                policy_kwargs = dict(net_arch=dict(pi=actor_net, qf=critic_net), activation_fn=af)
-
-                model = DDPG(MlpPolicy,
-                             env,
-                             learning_starts=3000,
-                             verbose=2,
-                             action_noise=action_noise,
-                             tensorboard_log="./test/",
-                             policy_kwargs=policy_kwargs,
-                             train_freq=1,
-                             gradient_steps=1
-                             )
-                model.learn(total_timesteps=50_000, tb_log_name=f"test", callback=callbacks)
-                utils.eval(PIAdaptivePT2(log=True), model, folder_name=RUN_NAME)
-                # #
-                # # # save model if you want to
-                model.save(f"eval\\{RUN_NAME}\\model")
+    model = DDPG(MlpPolicy,
+                 env,
+                 learning_starts=3000,
+                 verbose=2,
+                 action_noise=action_noise,
+                 tensorboard_log="./ddpg_direct_control/",
+                 policy_kwargs=policy_kwargs,
+                 train_freq=1,
+                 gradient_steps=1
+                 )
+    model.learn(total_timesteps=75_000, tb_log_name=f"{RUN_NAME}", callback=callbacks)
+    DirectControllerPT2(log=True).eval(model, folder_name=RUN_NAME)
+    # #
+    # # # save model if you want to
+    model.save(f"eval\\{RUN_NAME}\\model")
