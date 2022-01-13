cc115d8a70d90a68a53cadf4aa04d65369ed3b5e
halentin <74296706+halentin@users.noreply.github.com>
2022-01-13 11:36:30 +0100

    hihihoho

--- SysIdentPy/train.py
+++ SysIdentPy/train.py
@@ -5,11 +5,12 @@ import matplotlib.pyplot as plt
 
 from sysidentpy.neural_network import NARXNN
 from anarx_v2 import ANARX
+from narx import NARX
 
 from datatools import load_data
-MODEL_NAME  = "ANARX_20_20"
+MODEL_NAME  = "NARX_6_1_long_2"
 
-DATASET = "pt2.mat"
+DATASET = "newsensorlong.mat"
 
 writer = SummaryWriter(f"runs/{MODEL_NAME}")
 
@@ -27,12 +28,12 @@ def train(net, data, epochs, learning_rate):
     writer.add_scalar('Loss/Val', net.val_loss[epochs-1], net.net.global_epochs)
     fig = plt.figure()
     ax = fig.add_axes([0,0,1,1])
-    ax.bar(np.arange(1,21), net.net.weights_by_lag.numpy())
+    #ax.bar(np.arange(1,21), net.net.weights_by_lag.numpy())
     writer.add_figure('Lags/Weights', fig, net.net.global_epochs)
 
 current_net = pickle.load(open(f"SysIdentPy/models/{MODEL_NAME}.p", "rb" ))
-for i in range(10):
-    train(current_net, "pt2.mat", 10, 0.0001)
+for i in range(50):
+    train(current_net, DATASET, 3, 0.00001)
     
 if input("save? y/n\n") == "y":
     with open(f"SysIdentPy/models/{MODEL_NAME}.p", "wb") as f:
