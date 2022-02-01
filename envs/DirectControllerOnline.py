157f5f4444c85729cd467ea285147caca96aafbb
jubra97 <julius.brandl97@gmail.com>
2022-02-01 14:04:01 +0100

    use error instead of  and y

--- envs/DirectControllerOnline.py
+++ envs/DirectControllerOnline.py
@@ -35,8 +35,8 @@ class DirectControllerOnline(gym.Env):
         self.last_step_time = None
         self.last_reset_time = None
 
-        self.observation_space = gym.spaces.Box(low=np.array([-100]*6), high=np.array([100]*6),
-                                                shape=(6,),
+        self.observation_space = gym.spaces.Box(low=np.array([-100]*4), high=np.array([100]*4),
+                                                shape=(4,),
                                                 dtype=np.float32)
         self.action_space = gym.spaces.Box(low=np.array([-1]*1), high=np.array([1]*1), shape=(1,),
                                            dtype=np.float32)
@@ -71,6 +71,8 @@ class DirectControllerOnline(gym.Env):
         self.episode_log["obs"]["system_output_vel"] = []
         self.episode_log["obs"]["system_input"] = []
         self.episode_log["obs"]["input_vel"] = []
+        self.episode_log["obs"]["error"] = []
+        self.episode_log["obs"]["error_vel"] = []
         self.episode_log["rewards"]["summed"] = []
         self.episode_log["rewards"]["pen_error"] = []
         self.episode_log["rewards"]["pen_u_change"] = []
@@ -108,7 +110,7 @@ class DirectControllerOnline(gym.Env):
         self.last_step_time = time.perf_counter()
 
         # scale action
-        u = (action[0] + 1) * 250
+        u = (action[0] + 1) * 300
         u = np.clip(u, 0, 500)
 
         # do logging
@@ -176,7 +178,7 @@ class DirectControllerOnline(gym.Env):
             pen_action = 0
         else:
             action_change = abs(current_u - self.last_u)
-            pen_action = np.sqrt(action_change) * 5
+            pen_action = np.sqrt(action_change) * 3
 
         abs_error = abs(e)
         pen_error = np.abs(e)  # add penalty for high error between y and w
@@ -215,26 +217,39 @@ class DirectControllerOnline(gym.Env):
         if self.w.size < 2:
             print("Obs wrong")
             print(self.w)
-            return [0, 0, 0, 0, 0, 0]
+            return [0, 0, 0, 0]
 
         set_point = self.w[-1]
         set_point_vel = (self.w[-1] - self.w[-2]) * 1 / self.sample_freq
         system_output = self.y[-1]
         system_output_vel = (self.y[-1] - self.y[-2]) * 1 / self.sample_freq
         system_input = (self.u[-1] / 250) - 1
+        error = self.w[-1] - self.y[-1]
+        error_vel = ((self.w[-1] - self.y[-1]) - (self.w[-2] - self.y[-2])) * 1 / self.sample_freq
+
         if self.last_u is None or current_u is None:
             input_vel = 0
         else:
             input_vel = (current_u - self.last_u) * 1 / self.sample_freq
 
-        self.episode_log["obs"]["set_point"].append(set_point)
-        self.episode_log["obs"]["set_point_vel"].append(set_point_vel)
-        self.episode_log["obs"]["system_output"].append(system_output)
-        self.episode_log["obs"]["system_output_vel"].append(system_output_vel)
+        # self.episode_log["obs"]["set_point"].append(set_point)
+        # self.episode_log["obs"]["set_point_vel"].append(set_point_vel)
+        # self.episode_log["obs"]["system_output"].append(system_output)
+        # self.episode_log["obs"]["system_output_vel"].append(system_output_vel)
+        # self.episode_log["obs"]["system_input"].append(system_input)
+        # self.episode_log["obs"]["input_vel"].append(input_vel)
+        #
+        # obs = [set_point, system_input, system_output, set_point_vel, input_vel, system_output_vel]
+        # obs[2] = 0
+        # obs[-1] = 0
+
+        self.episode_log["obs"]["error"].append(error)
+        self.episode_log["obs"]["error_vel"].append(error_vel)
         self.episode_log["obs"]["system_input"].append(system_input)
         self.episode_log["obs"]["input_vel"].append(input_vel)
 
-        obs = [set_point, system_input, system_output, set_point_vel, input_vel, system_output_vel]
+        obs = [error, system_input, error_vel, input_vel]
+
 
         return np.array(obs)
 
