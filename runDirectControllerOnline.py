14c78c0992fc841e39ab6f84883294e0ea5d7779
jubra97 <julius.brandl97@gmail.com>
2022-03-18 12:26:19 +0100

    update model run

--- runDirectControllerOnline.py
+++ runDirectControllerOnline.py
@@ -7,7 +7,7 @@ env = DirectControllerOnline(online_connection, output_freq=200)
 
 
 
-model = DDPG.load(r"C:\Users\brandlju\PycharmProjects\Projektmodul\eval\direct_with_error\end_model2.zip", env)
+model = DDPG.load(r"C:\Users\brandlju\PycharmProjects\Projektmodul\working_online_agents\1\end_model2.zip", env)
 
 
 while True:
