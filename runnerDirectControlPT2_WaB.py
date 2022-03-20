4c2d2edf51d7c2ff9a726a459bf3543de45a6205
Julius <julius.brandl97@gmail.com>
2022-03-20 22:29:33 +0100

    update for obs/reward tests

--- runnerDirectControlPT2_WaB.py
+++ runnerDirectControlPT2_WaB.py
@@ -1,32 +1,34 @@
+import multiprocessing
+
 import numpy as np
-import torch as th
-import os
 import wandb
-from wandb.integration.sb3 import WandbCallback
-from stable_baselines3 import DDPG, TD3
+from stable_baselines3 import DDPG
 from stable_baselines3.common.callbacks import CallbackList
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.noise import NormalActionNoise
 from stable_baselines3.common.vec_env import SubprocVecEnv
-from stable_baselines3.ddpg.policies import MlpPolicy
-from torch import nn
+from wandb.integration.sb3 import WandbCallback
+
 from CustomEvalCallback import CustomEvalCallback
 from envs.DirectControllerSim import DirectControllerSim
 import custom_policy_no_bias
-import multiprocessing
+from torch import nn
 
 if __name__ == "__main__":
-
     hyperparameter_defaults = {
         "actor_layers": 2,
-        "actor_layer_width": 5,
         "actor_activation_fun": "nn.Tanh()",
-        "actor_end_activation_fun": "nn.Hardtanh()",
-        "critic_layers": 1,
+        "critic_layers": 2,
         "critic_layer_width": 50,
         "critic_activation_fun": "nn.ReLU()",
-        "critic_bias": True
+        "obs_i": True,
+        "obs_d": True,
+        "obs_input_vel": True,
+        "obs_output_vel": False,
+        "obs_output": False,
+        "obs_input": False,
+        "reward_fun": "discrete_u_pen_dep_on_error",
     }
 
     run = wandb.init(
@@ -39,9 +41,18 @@ if __name__ == "__main__":
     config = wandb.config
     print(config)
 
-
-    print(multiprocessing.cpu_count()-1)
-    env = make_vec_env(DirectControllerSim, multiprocessing.cpu_count()-1, vec_env_cls=SubprocVecEnv)  # create learning env
+    obs_config = {
+        "i": config["obs_i"],
+        "d": config["obs_d"],
+        "input_vel": config["obs_input_vel"],
+        "output_vel": config["obs_output_vel"],
+        "output": config["obs_output"],
+        "input": config["obs_input"]
+    }
+    print(multiprocessing.cpu_count() - 1)
+    env = make_vec_env(DirectControllerSim, multiprocessing.cpu_count() - 1, vec_env_cls=SubprocVecEnv,
+                       env_kwargs={"obs_config": obs_config,
+                                   "reward_function": config["reward_fun"]})  # create learning env
     # env = DirectControllerSim()
 
     # create action noise
@@ -49,7 +60,7 @@ if __name__ == "__main__":
     action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=float(0.1) * np.ones(n_actions))
 
     # create eval callback
-    online_eval_env = DirectControllerSim(log=True)  # create eval env
+    online_eval_env = DirectControllerSim(log=True, obs_config=obs_config, reward_function=config["reward_fun"])  # create eval env
     online_eval_env = Monitor(online_eval_env)
     eval_callback = CustomEvalCallback(online_eval_env, eval_freq=3000, deterministic=True,
                                        best_model_save_path=f"models/{run.id}\\best_model")
@@ -60,13 +71,10 @@ if __name__ == "__main__":
     # # use DDPG and create a tensorboard
     # # start tensorboard server with tensorboard --logdir ./{tensorboard_log}/
     policy_kwargs = {"actor_layers": config["actor_layers"],
-                     "actor_layer_width": config["actor_layer_width"],
                      "actor_activation_fun": eval(config["actor_activation_fun"]),
-                     "actor_end_activation_fun": eval(config["actor_end_activation_fun"]),
                      "critic_layers": config["critic_layers"],
                      "critic_layer_width": config["critic_layer_width"],
                      "critic_activation_fun": eval(config["critic_activation_fun"]),
-                     "critic_bias": config["critic_bias"],
                      }
 
     model = DDPG("CustomTD3Policy",
@@ -81,7 +89,10 @@ if __name__ == "__main__":
                  learning_rate=1e-3
                  )
     model.learn(total_timesteps=150_000, tb_log_name=f"{run.id}", callback=callbacks)
-    eval_info = DirectControllerSim(log=True).eval(model, folder_name=f"{run.dir}\custom_eval")
+    eval_info = DirectControllerSim(log=True,
+                                    obs_config=obs_config,
+                                    reward_function=config["reward_fun"]
+                                    ).eval(model, folder_name=f"{run.dir}\custom_eval")
 
     wandb.log(eval_info)
     wandb.finish()
