7e51303c764618669d8d5356272783a99edff3d8
Julius <julius.brandl97@gmail.com>
2022-07-05 22:52:08 +0200

    update path

--- ergebnisse_chapter_runner/runnerDirectControlSimBasicConfigWandB.py
+++ ergebnisse_chapter_runner/runnerDirectControlSimBasicConfigWandB.py
@@ -66,8 +66,8 @@ if __name__ == "__main__":
     }
 
     rl_options = {
-        "save_path": "wandb_observations",
-        "tensorboard_log_name": "tensorboard_wandb_observations",
+        "save_path": "wandb_base_config",
+        "tensorboard_log_name": "tensorboard_base_config",
         "cpu_cores": 3,
         "timesteps": 300_000,
         "action_noise": (0.1, 0.0003, 250_000)
